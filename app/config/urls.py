3a4843635c2f5c2216bc0e3002557fa78380befe
assisfaavf <assisfaavfp@gmail.com>
2026-04-07 15:52:05 -0300

    fix: corrigir entrega do favicon

--- app/config/urls.py
+++ app/config/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico"), permanent=False),
+    ),
     path("", include("billing.urls")),
 ]
